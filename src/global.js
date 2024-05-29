f53ef201aab009843119d4b281d0dd2112056b5a
Peroche95 <marllonperohe@gmail.com>
2024-05-29 10:07:47 -0300

    primeiro commit

---
+++ src/global.js
@@ -0,0 +1,16 @@
+import  { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    background-color : #1D2128;
+
+}
+
+hr{
+    background-color: rgba(229, 229, 229, 0.2);
+    border: 1px solid rgba(229, 229, 229, 0.2);
+}
+
+`
